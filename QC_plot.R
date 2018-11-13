9053b0f973484d5b0d8e935346a60ef1f5da7061
ryandkuster <ryan.kuster@gmail.com>
2018-11-13 13:43:24 -0500

    added R plotting script

---
+++ QC_plot.R
@@ -0,0 +1,41 @@
+library(plyr)
+library(car)
+library(MASS)
+library(data.table)
+library(ggplot2)
+library(reshape)
+
+setwd ("/home/rkuster/Desktop/QC_plotting")
+
+xyz <- read.table("quals.txt", header=F, sep=",")
+names(xyz) <- gsub("V","", names(xyz))
+boxplot <- ggplot(stack(xyz), aes(x = ind, y=values), stat='identity')+
+  geom_boxplot(fill="white", colour="cornflowerblue",
+               outlier.alpha = 0.01, outlier.colour="tomato", outlier.size=1.0)+
+  coord_flip()+
+  scale_y_continuous(expand=c(0,0)) +
+  theme(axis.text.x=element_text(colour="cornflowerblue", size=18),
+        axis.text.y=element_text(colour="cornflowerblue", size=18),
+        axis.title=element_text(size=24)) +
+  xlab("Quality Scores") +
+  ylab("Frequency")
+ggsave(filename="Frequency_QC_Score.tiff", plot=boxplot, width=15, height= 15, dpi=600, compression = "lzw")
+gc
+
+#Now transform data
+qc <- reshape(xyz, direction="long", varying=list(c(1:ncol(xyz))), v.names="count")
+qc$score <- qc$time-1
+qc <- rename(qc, c("id"="position"))
+qc <- subset(qc, select=c("position","score","count"))
+boxplot <- ggplot(qc, aes(x = position, y=score, weight=count, group=position), stat='identity')+
+  geom_boxplot(fill="white", colour="cornflowerblue",
+               outlier.alpha = 0.01, outlier.colour="tomato", outlier.size=1.0)+
+  scale_x_continuous(expand=c(0,0), breaks=seq(0,nrow(xyz),by=5)) +
+  scale_y_continuous(expand=c(0,0), breaks=seq(0,40, by=2)) +
+  theme(axis.text.x=element_text(colour="cornflowerblue", size=12),
+        axis.text.y=element_text(colour="cornflowerblue", size=12),
+        axis.title=element_text(size=18)) +
+  xlab("Read Position") +
+  ylab("Quality Scores")
+ggsave(filename="QC_plot.tiff", plot=boxplot, width=15, height= 15, dpi=600, compression = "lzw")
+gc
