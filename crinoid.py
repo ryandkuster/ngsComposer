bafca3886a2217506b3129957af11c2b217a721c
ryandkuster <ryan.kuster@gmail.com>
2019-02-12 17:56:58 -0500

    minor changes to crinoid

--- crinoid.py
+++ crinoid.py
@@ -17,9 +17,9 @@ def crinoid_init(in1, proj_dir):
     out1 = proj_dir + '/nucleotides_' + os.path.basename(in1) + '.txt'
     out2 = proj_dir + '/qscores_' + os.path.basename(in1) + '.txt'
     scores = open(os.path.dirname(os.path.abspath(__file__)) + '/scores.txt').read().split()
-    val = dict(zip(scores[:41],scores[-int(len(scores)/2):-int(len(scores)/2)+41]))
+    score_dict = dict(zip(scores[:41],scores[-int(len(scores)/2):-int(len(scores)/2)+41]))
     max_len = max_len_count(in1)
-    crinoid(in1, out1, out2, max_len, val)
+    crinoid(in1, out1, out2, max_len, score_dict)
 
 
 def max_len_count(in1):
@@ -36,11 +36,12 @@ def max_len_count(in1):
     return max_len
 
 
-def crinoid(in1, out1, out2, max_len, val):
+def crinoid(in1, out1, out2, max_len, score_dict):
     with open(in1) as f, open(out1, "w") as o1, open(out2, "w") as o2:
+        print('processing reads')
         y = 0
         base_dict = {i: {'A': 0, 'C': 0, 'G': 0, 'T' : 0, 'N': 0} for i in range(max_len)}
-        score_dict = {i: {j: 0 for j in val.keys()} for i in range(max_len)}
+        qual_dict = {i: {j: 0 for j in score_dict.keys()} for i in range(max_len)}
         for line in f:
             y += 1
             if y == 2:
@@ -48,17 +49,25 @@ def crinoid(in1, out1, out2, max_len, val):
                     base_dict[i][base] +=1
             if y == 4:
                 for i, base in enumerate(line.rstrip()):
-                    score_dict[i][base] +=1
+                    qual_dict[i][base] +=1
                 y = 0
-        output_prep(base_dict)
+        base_mx = [[0] * len(base_dict[0]) for i in range(max_len)]
+        score_mx = [[] for i in range(max_len)]
+        score_mx = output_prep(qual_dict, score_mx, score_dict)
+        print(score_mx)
 
 
-def output_prep(dictionary):
-    for k, v in dictionary.items():
-        if isinstance(v, dict):
-            output_prep(v)
+def output_prep(dict1, mx, dict2):
+    for k1, v1 in dict1.items():
+        if isinstance(v1, dict):
+            output_prep(v1, mx, dict2)
         else:
-            print(k)
+            for k2, v2 in dict2.items():
+                if k1 == k2:
+                    print(k1)
+                    print(k2)
+                    mx[int(v2)].append(v1)
+    return mx
 
 
 if __name__ == "__main__":
