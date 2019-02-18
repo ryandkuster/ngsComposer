819d38eaed351d22ba3ddc52b48aa95fdf7723ac
ryandkuster <ryan.kuster@gmail.com>
2019-02-18 16:18:23 -0500

    fixed scallop, updated R plot

--- tools/qc_plot.R
+++ tools/qc_plot.R
@@ -1,42 +1,43 @@
-library(plyr)
-library(car)
-library(MASS)
-library(data.table)
 library(ggplot2)
-library(reshape)
 
-setwd ("/home/rkuster/Desktop/QC_plotting")
-
-xyz <- read.table("quals.txt", header=F, sep=",")
-names(xyz) <- gsub("V","", names(xyz))
-boxplot <- ggplot(stack(xyz), aes(x = ind, y=values), stat='identity')+
+setwd ("C:\\Users\\rkuster\\vm_shared_data")
+qscores <- read.table("qscores_1_R1.fastq.txt", header=F, sep=",") # create table object
+names(qscores) <- gsub("V","", names(qscores))                                                   # get rid of the 'v' that R attaches to its header
+sum <- format(round(as.numeric(sum(qscores[1,])), 1), nsmall=0, big.mark=",")
+xyz <- stack(qscores)
+for (i in levels(xyz$ind)){
+  j = strtoi(i) - 1
+  print(j)
+  levels(xyz$ind)[levels(xyz$ind) == i] <- toString(j)
+}
+boxplot <- ggplot(xyz, aes(x = ind, y=values), stat='identity')+
   geom_boxplot(fill="white", colour="cornflowerblue",
                outlier.alpha = 0.25, outlier.colour="tomato", outlier.size=2.0)+
-  coord_flip()+
-  scale_x_discrete((xyz$ind)+1, limits=c(1:40))+
-  scale_y_continuous(expand=c(0,0)) +
   theme(axis.text.x=element_text(colour="cornflowerblue", size=18),
         axis.text.y=element_text(colour="cornflowerblue", size=18),
         axis.title=element_text(size=24)) +
+  theme_minimal()+
+  coord_flip()+
   xlab("Quality Scores") +
-  ylab("Frequency")
+  ylab(paste("Frequency (Total Number of reads = ",sum,")", sep=""))
+boxplot
 ggsave(filename="Frequency_QC_Score.tiff", plot=boxplot, width=15, height= 15, dpi=600, compression = "lzw")
-gc
+gc()
 
 #Now transform data
-qc <- reshape(xyz, direction="long", varying=list(c(1:ncol(xyz))), v.names="count")
+qc <- reshape(qscores, direction="long", varying=list(c(1:ncol(qscores))), v.names="count")
 qc$score <- qc$time-1
-qc <- rename(qc, c("id"="position"))
-qc <- subset(qc, select=c("position","score","count"))
+colnames(qc)[colnames(qc)=="id"] <- "position"
+var <- c("position","score","count")
+qc <- qc[var]
 boxplot <- ggplot(qc[which(qc$count>0),], aes(x = position, y=score, weight=count, group=position), stat='identity')+
-  geom_boxplot(fill="white", colour="cornflowerblue",
-               outlier.alpha = 0.2, outlier.colour="tomato", outlier.size=2)+
-   scale_x_continuous(expand=c(0,0), limit=c(0,nrow(xyz)), breaks=seq(0,nrow(xyz),by=1)) +
-  scale_y_continuous(expand=c(0,0), limit=c(0,40), breaks=seq(0,40, by=2)) +
-  theme(axis.text.x=element_text(colour="cornflowerblue", size=8),
-        axis.text.y=element_text(colour="cornflowerblue", size=8),
-        axis.title=element_text(size=12)) +
-  xlab("Read Position") +
-  ylab("Quality Scores")
-ggsave(filename="QC_plot.tiff", plot=boxplot, width=15, height= 5, dpi=600, compression = "lzw")
-gc
+  geom_boxplot(fill="white", colour="cornflowerblue", lwd = 0.25,
+               outlier.alpha = 0.2, alpha=0.2, outlier.colour="tomato", outlier.size=1.25)+
+  scale_x_continuous(expand=c(0,0), limit=c(0,nrow(qscores)+1), breaks=seq(0,nrow(qscores)+1,by=5), 
+                     sec.axis = dup_axis(name = NULL)) +
+  scale_y_continuous(expand=c(0,0), limit=c(0,41), breaks=seq(0,41, by=2), sec.axis = dup_axis(name = NULL)) +
+  theme_classic()+
+  xlab(paste("Read Position (Total Number of reads = ",sum,")", sep="")) +
+  ylab("Quality Scores (phred+33)")
+ggsave(filename="QC_boxplot.tiff", plot=boxplot, width=15, height= 5, dpi=600, compression = "lzw")
+gc()
--- tools/scallop.py
+++ tools/scallop.py
@@ -9,20 +9,20 @@ def scallop_main():
     in1 = sys.argv[1]
     front_trim = int(sys.argv[2])
     back_trim = int(sys.argv[3])
-    out1 = int(sys.argv[4])
+    out1 = sys.argv[4]
     proj_dir = os.path.abspath(in1)
     scallop(in1, front_trim, back_trim, proj_dir, out1)
 
 
 def scallop(in1, front_trim, back_trim, proj_dir, out1):
     '''
-    composer entry point to scallop
+    trim defined base numbers from the front or end of reads
     '''
     if back_trim == 0:
         back_trim = None
     else:
         back_trim = - back_trim
-    with open(in1) as f, open(proj_dir + '/' + out1, 'w') as o:
+    with open(in1) as f, open(out1, 'w') as o:
         for i, line in enumerate(f):
             i += 1
             if i % 2 == 0:
@@ -33,7 +33,10 @@ def scallop(in1, front_trim, back_trim, proj_dir, out1):
 
 
 def scallop_comp(front_trim, back_trim, proj_dir, in1):
-    out1 = os.path.basename(in1)
+    '''
+    composer entry point to scallop
+    '''
+    out1 = proj_dir + '/' + os.path.basename(in1)
     scallop(in1, front_trim, back_trim, proj_dir, out1)
 
 
